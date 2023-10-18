8f7e8c27d1b6805d7fe45c67d19e89b4b1b3db57
SHIBANSU MANDAL <118418396+shibansu2003@users.noreply.github.com>
2023-10-18 23:31:04 +0530

    Add files via upload

---
+++ Access Specifier/accessspecifier.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+class MyClass {
+  public:    // Public access specifier
+    int x;   // Public attribute
+  private:   // Private access specifier
+    int y;   // Private attribute
+};
+
+int main() {
+  MyClass myObj;
+  myObj.x = 25;  // Allowed (public)
+  //myObj.y = 50;   Not allowed (private)
+  return 0;
+}
+//Shibansu Mandal
