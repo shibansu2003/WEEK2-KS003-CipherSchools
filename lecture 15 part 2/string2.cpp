2e63bf73f345276ec240bc8b8947d10329a2eae4
SHIBANSU MANDAL <118418396+shibansu2003@users.noreply.github.com>
2023-10-18 23:32:52 +0530

    Add files via upload

---
+++ lecture 15 part 2/string2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	string name;  //string is a datatype
+	cin>>name;
+	for(int i=0;i<name.length();i++){
+		cout<<name[i]<<endl;
+	}
+	return 0;
+}
+//Shibansu Mandal
